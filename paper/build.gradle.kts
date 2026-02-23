b90daca74232e24df9738fbe4425baea434d878f
Hosairis <amir.h.rafiei2001@gmail.com>
2026-02-23 15:14:29 +0330

    - update kotlin dependency - update paper dependency - update bstats dependency - update gradle version - update shadow plugin - update bukkit-factory plugin - update run-paper plugin
    - stop using paperweight
    - implement bstats by default
    - minimize output jar
    - set output jar name
    - update viaversion and viabackwards plugin versions

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.paperweight)         // Paperweight plugin for Minecraft server development
     alias(libs.plugins.bukkit.factory)      // Bukkit resource factory plugin for generating plugin.yml at build time
     alias(libs.plugins.run.paper)           // The run-task plugin for running a test server and testing the plugin
 }
@@ -9,33 +8,35 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle(libs.versions.paper) // Paper development bundle
+    compileOnly(libs.paper)
 
-    // implementation(libs.bstats)
+     implementation(libs.bstats)
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17) // Use Java 17 toolchain
+    toolchain.languageVersion = JavaLanguageVersion.of(21) // Use Java 17 toolchain
 }
 
 tasks {
     runServer {
-        minecraftVersion("1.18.2") // Configure the Minecraft server version.
+        minecraftVersion("1.21.11")
         downloadPlugins {
-            // Download ViaVersion and ViaBackwards plugins.
-            modrinth("viaversion", "5.2.2-SNAPSHOT+686")
-            modrinth("viabackwards", "5.2.2-SNAPSHOT+391")
+            modrinth("viaversion", "5.7.2-SNAPSHOT+925")
+            modrinth("viabackwards", "5.7.2-SNAPSHOT+548")
         }
     }
 
-//    shadowJar {
-//        relocate("org.bstats", "example.plugin.template.libs.bstats")
-//    }
+    shadowJar {
+        relocate("org.bstats", "example.plugin.template.libs.bstats")
+
+        archiveFileName.set("${rootProject.name}-${project.version}.jar")
+        minimize()
+    }
 }
 
 bukkitPluginYaml {
     name = "MCTemplate"
     main = "example.plugin.template.Template"
-    authors.add("Author")
-    apiVersion = "1.18"
+    authors.add("Hosairis")
+    apiVersion = "1.21"
 }
--- paper/src/main/kotlin/example/plugin/template/Template.kt
+++ paper/src/main/kotlin/example/plugin/template/Template.kt
@@ -1,27 +1,28 @@
 package example.plugin.template
 
+import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
 class Template: JavaPlugin() {
     
     companion object {
         private lateinit var instance: Template
-//        private lateinit var metrics: Metrics
 
         fun getInst(): Template {
             return instance
         }
     }
+    private lateinit var metrics: Metrics
 
     // Called when the plugin is enabled (initialize resources, register events/commands).
     override fun onEnable() {
         instance = this
 
-//        metrics = Metrics(getInst(), /* bStats ID Here */)
+        metrics = Metrics(getInst(), 123) // Remember to change the plugin ID
     }
 
     // Called when the plugin is disabled (cleanup resources, save data).
     override fun onDisable() {
-//        metrics.shutdown()
+        metrics.shutdown()
     }
 }
